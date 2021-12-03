4d7a1afa5ce66ea6693a7c3d6e066544f46d23ac
Phoque <lafont_jordan@hotmail.fr>
2021-12-03 10:32:01 +0100

    Ropes, link anchor to candy, trail renderer to follow input

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Update is called once per frame
+    [SerializeField]
+    TrailRenderer tr;
+
     void Update()
     {
-        Vector2 vTouchPos;
-        for (int i = 0; i < Input.touchCount; i++)
+        Vector2 touchPos = Vector2.zero;
+        Vector2 touchPosOld = Vector2.zero;
+        if (Input.touchCount > 0)
         {
-            Debug.Log("Touch!");
-            vTouchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
-            Collider2D col = Physics2D.OverlapPoint(vTouchPos);
-            Debug.Log("Collided!");
-            if (col != null && col.CompareTag("RopeSegment"))
-			{
-                Debug.Log("Rope destroyed!");
-                Destroy(col.gameObject);
+            Touch touch = Input.GetTouch(0);
+            touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosOld = Camera.main.ScreenToWorldPoint(touch.position - touch.deltaPosition);
+            List<RaycastHit2D> hits = new List<RaycastHit2D>();
+            int a = Physics2D.Raycast(touchPos, touchPosOld - touchPos, new ContactFilter2D(), hits, Vector2.Distance(touchPos, touchPosOld));
+            Debug.DrawRay(touchPos, touchPosOld-touchPos, Color.red);
+            if (a > 0)
+            {
+				foreach (var hit in hits)
+				{
+					if (hit.collider.CompareTag("RopeSegment"))
+					{
+                        Destroy(hit.collider.gameObject);
+                    }
+                }
+			}
+            if ((touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary))
+            {
+				transform.position = touchPos;
+                if (tr.enabled == false)
+				{
+					tr.enabled = true;
+				}
+            }
+            else
+            {
+                tr.Clear();
+                tr.enabled = false;
             }
         }
     }
